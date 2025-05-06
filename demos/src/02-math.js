aa8bf3bf0a0fdc8dd03a79734d7db2c7c7b1e9bf
OscarS05 <santiagom0509@hotmail.com>
2025-05-06 12:25:38 -0500

    Creación de pruebas unitarias y se añadió eslint

---
+++ demos/src/02-math.js
@@ -0,0 +1,23 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if(a === 0 || b === 0) return null;
+  return a / b;
+}
+
+function average(a, b){
+  return (a + b) / 2;
+}
+
+module.exports = {
+  sum,
+  multiply,
+  divide,
+  average
+};
