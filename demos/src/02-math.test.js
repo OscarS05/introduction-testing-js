7f54c68a182347762fe863bc1c3747044b4d7414
OscarS05 <santiagom0509@hotmail.com>
2025-05-06 18:35:19 -0500

    Implementación de pruebas con describe en Class Person

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -2,36 +2,46 @@ const {
   sum, divide, multiply, average,
 } = require('./02-math');
 
-test('adds 1 + 3 should be 4', () => {
-  const rta = sum(1, 3);
-  expect(rta).toBe(4);
-});
+describe('Test for math', () => {
+  describe('Test for sum', () => {
+    test('adds 1 + 3 should be 4', () => {
+      const rta = sum(1, 3);
+      expect(rta).toBe(4);
+    });
+  });
 
-test('should be 4', () => {
-  const rta = multiply(2, 2);
-  expect(rta).toBe(4);
-});
+  describe('Test for multiply', () => {
+    test('should be 4', () => {
+      const rta = multiply(2, 2);
+      expect(rta).toBe(4);
+    });
+  });
 
-test('should divide', () => {
-  const rta = divide(6, 3);
-  expect(rta).toBe(2);
+  describe('Test for divide', () => {
+    test('should divide', () => {
+      const rta = divide(6, 3);
+      expect(rta).toBe(2);
 
-  const rta2 = divide(5, 2);
-  expect(rta2).toBe(2.5);
-});
+      const rta2 = divide(5, 2);
+      expect(rta2).toBe(2.5);
+    });
 
-test('should divide for zero', () => {
-  const rta = divide(6, 0);
-  expect(rta).toBeNull();
+    test('should divide for zero', () => {
+      const rta = divide(6, 0);
+      expect(rta).toBeNull();
 
-  const rta2 = divide(5, 0);
-  expect(rta2).toBeNull();
-});
+      const rta2 = divide(5, 0);
+      expect(rta2).toBeNull();
+    });
+  });
 
-test('should average', () => {
-  const rta = average(4, 8);
-  expect(rta).toBe(6);
+  describe('Test for average', () => {
+    test('should average', () => {
+      const rta = average(4, 8);
+      expect(rta).toBe(6);
 
-  const rta2 = average(15, 90);
-  expect(rta2).toBe(52.5);
+      const rta2 = average(15, 90);
+      expect(rta2).toBe(52.5);
+    });
+  });
 });
--- demos/src/05-setup.test.js
+++ demos/src/05-setup.test.js
@@ -1,36 +1,36 @@
 describe('group 1', () => {
   beforeAll(() => {
-    console.log('beforeAll');
+    // console.log('beforeAll');
   });
   afterAll(() => {
-    console.log('afterAll');
+    // console.log('afterAll');
   });
   beforeEach(() => {
-    console.log('beforeEach');
+    // console.log('beforeEach');
   });
   afterEach(() => {
-    console.log('afterEach');
+    // console.log('afterEach');
   });
 
   test('Case 1', () => {
-    console.log('case 1');
+    // console.log('case 1');
     expect(1 + 1).toBe(2);
   });
   test('Case 2', () => {
-    console.log('case 2');
-    expect(1 + 2).toBe(2);
+    // console.log('case 2');
+    expect(1 + 2).toBe(3);
   });
 
   describe('group 1.2', () => {
     beforeAll(() => {
-      console.log('beforeAll 1.2');
+      // console.log('beforeAll 1.2');
     });
     test('Case 3', () => {
-      console.log('case 3');
-      expect(1 + 3).toBe(2);
+      // console.log('case 3');
+      expect(1 + 3).toBe(4);
     });
     test('Case 4', () => {
-      console.log('case 4');
+      // console.log('case 4');
       expect(1 + 4).toBe(5);
     });
   });
