ef44882e1cf6911b32ab5d020a66514eac34f9ea
OscarS05 <santiagom0509@hotmail.com>
2025-05-07 11:12:25 -0500

    Carpeta /api lista para testear

---
+++ api/.eslintrc.js
@@ -0,0 +1,19 @@
+// To install packages could be like that:
+// npm install --save-dev eslint@8.57.0 eslint-config-airbnb-base@15.0.0 eslint-plugin-import@2.29.1
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+  },
+};
