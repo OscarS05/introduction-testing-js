8742015387e2adb0f80169153a07dc8f12621872
OscarS05 <santiagom0509@hotmail.com>
2025-05-07 17:33:13 -0500

    Prueba de e2e realizada

--- api/e2e/books.e2e.js
+++ api/e2e/books.e2e.js
@@ -1,32 +1,50 @@
-const mockGetAll = jest.fn();
 const request = require('supertest');
+const { MongoClient } = require('mongodb');
 
 const createApp = require('../src/app');
-const { generateManyBooks } = require('../src/fakes/book.fake');
+const { config } = require('../src/config/index');
+// const { generateManyBooks } = require('../src/fakes/book.fake');
 
-jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
-  getAll: mockGetAll,
-  create: () => {},
-})));
+const DB_NAME = config.dbName;
+const MONGO_URI = config.dbUrl;
 
 describe('Test for books endpoint', () => {
   let app = null;
   let server = null;
+  let database = null;
 
-  beforeAll(() => {
+  beforeAll(async () => {
     app = createApp();
     server = app.listen(3001);
+    const client = new MongoClient(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+    database = client.db(DB_NAME);
   });
 
   afterAll(async () => {
     await server.close();
+    await database.dropDatabase();
   });
 
   describe('Test for GET /api/v1/books', () => {
-    test('should return books', () => {
+    test('should return books', async () => {
       // Arrange
-      const fakeBooks = generateManyBooks(3);
-      mockGetAll.mockResolvedValue(fakeBooks);
+      const seedData = await database.collection('books').insertMany([
+        {
+          name: 'Book1',
+          year: 1998,
+          author: 'oscar',
+        },
+        {
+          name: 'Book2',
+          year: 1998,
+          author: 'oscar',
+        },
+      ]);
+      console.log(seedData);
       // Act
       return request(app)
         .get('/api/v1/books')
@@ -34,7 +52,7 @@ describe('Test for books endpoint', () => {
         .then(({ body }) => {
           console.log(body);
           // Assert
-          expect(body.length).toEqual(fakeBooks.length);
+          expect(body.length).toEqual(seedData.insertedCount);
         });
     });
   });
